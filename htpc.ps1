91b9c417f4e2c4243bc06b2ac04a9ce5e3d7d5ef
Mohammed Almusaddar <my.almusaddar@gmail.com>
2017-01-03 13:43:17 +0200

    automate nssm

--- htpc.ps1
+++ htpc.ps1
@@ -1,5 +1,6 @@
 ﻿iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+choco feature enable -n=allowGlobalConfirmation
 choco install -y python2
 choco install -y pip
 choco install -y git
@@ -8,9 +9,26 @@ choco install -y nssm
 choco install -y qbittorrent
 choco install -y openhardwaremonitor
 choco install -y smartmontools
+choco install -y sabnzbd
+choco install -y couchpotato
+choco install -y sonarr
+choco install -y kodi
+choco install -y plexmediaserver
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
 pip install Pillow
-pip install psutil 
-git clone https://github.com/Hellowlol/HTPC-Manager c:\HTPCManager
-nssm install HTPCManager
-net start HTPCManager
+pip install psutil
+
+git clone https://github.com/Hellowlol/HTPC-Manager c:\HTPC\HTPCManager
+nssm install HTPCManager c:\python27\python.exe "c:\HTPC\HTPCManager\Htpc.py"
+nssm set HTPCManager AppDirectory "c:\HTPC\HTPCManager"
+nssm set HTPCManager Start SERVICE_AUTO_START
+nssm start HTPCManager
+
+git clone https://github.com/rembo10/headphones.git c:\HTPC\Headphones
+nssm install Headphones c:\python27\python.exe "c:\HTPC\Headphones\headphones.py"
+nssm set Headphones AppDirectory "c:\HTPC\Headphones"
+nssm set Headphones Start SERVICE_AUTO_START
+nssm start Headphones
+
+dir >$null 2>&1
